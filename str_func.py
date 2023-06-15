a5d72a8674c092dafe215b538038540ed7b52d31
Inna <ighaidiei@bk.ru>
2023-06-15 12:50:41 +0200

    create convert_to_title_str function in str_func.py

--- str_func.py
+++ str_func.py
@@ -3,7 +3,7 @@ def convert_to_upper_str(plain_text: str) -> str:
     return plain_text.upper()
 
 
-def convert_to_capitalize_str(plain_text: str) -> str:
+def convert_to_title_str(plain_text: str) -> str:
     '''Преобразование каждого слова в специальный вид:
     каждое слово стоки plain_text начинается с заглавной буквы'''
-    return plain_text.capitalize()
+    return plain_text.title()
