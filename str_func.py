d72ef56fae38e25543211ae976cae7d5614a7749
Inna <ighaidiei@bk.ru>
2023-06-15 13:00:47 +0200

    add doctring to convert_to_upper_str

--- str_func.py
+++ str_func.py
@@ -1,9 +1,3 @@
 def convert_to_upper_str(plain_text: str) -> str:
     '''Преобразование всех букв в троке в зглавные'''
     return plain_text.upper()
-
-
-def convert_to_capitalize_str(plain_text: str) -> str:
-    '''Преобразование каждого слова в специальный вид:
-    каждое слово стоки plain_text начинается с заглавной буквы'''
-    return plain_text.capitalize()
